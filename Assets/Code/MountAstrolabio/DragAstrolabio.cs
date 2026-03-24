f7fb5bcaad56f59c25ad4f9244fc4aeee0883023
Raquel Casa <44086643+raquelesilva@users.noreply.github.com>
2026-03-24 00:49:57 +0000

    Tentativa de drag e rotate

--- Assets/Code/MountAstrolabio/DragAstrolabio.cs
+++ Assets/Code/MountAstrolabio/DragAstrolabio.cs
@@ -1,9 +1,40 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class DragAstrolabio : MonoBehaviour
 {
-    #region Input Actions
+    #region Variables
+    private Vector3 offset;
+    private Camera camera;
+    #endregion
+
+    private void Start()
+    {
+        camera = Camera.main;
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("MouseDown");
+        offset = transform.position - MouseWorldPosition();
+        Debug.Log("transform.position: " + transform.position);
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = MouseWorldPosition() + offset;
+        Debug.Log("transform.position: " + transform.position);
+    }
+
+    protected virtual Vector3 MouseWorldPosition()
+    {
+        var mouseScreenPos = Input.mousePosition;
+        mouseScreenPos.x = camera.WorldToScreenPoint(transform.position).x;
+        return camera.ScreenToWorldPoint(mouseScreenPos);
+    }
+
+   /* #region Input Actions
     [SerializeField]
     private InputActionAsset inputActions;
 
@@ -22,12 +53,18 @@ public class DragAstrolabio : MonoBehaviour
     #region Variables
 
     private bool moveAllowed;
+    private Vector3 curScreenPos;
 
     private Camera camera;
 
-    [SerializeField] private float speed;
-
-    [SerializeField] private bool inverted;
+    private Vector3 WorldPos
+    {
+        get
+        {
+            float z = camera.WorldToScreenPoint(transform.position).z;
+            return camera.ScreenToWorldPoint(curScreenPos + new Vector3(0, 0, z));
+        }
+    }
 
     #endregion
 
@@ -58,37 +95,49 @@ public class DragAstrolabio : MonoBehaviour
 
     protected virtual void OnLeftClickPressed(InputAction.CallbackContext context)
     {
+        Debug.Log("MouseDown");
         if (context.started || context.performed)
         {
             moveAllowed = true;
-            Cursor.lockState = CursorLockMode.Locked;
+            offset = transform.position - GetMouseLookInput();
+            Debug.Log("offset: " + offset);
+            //Cursor.lockState = CursorLockMode.Locked;
         }
         else if (context.canceled)
         {
             moveAllowed = false;
-            Cursor.lockState = CursorLockMode.None;
+            //Cursor.lockState = CursorLockMode.None;
         }
     }
 
-    protected virtual Vector2 GetMouseLookInput()
+    protected virtual Vector3 GetMouseLookInput()
     {
-        if (mouseLookInputAction != null)
-        {
-            return mouseLookInputAction.ReadValue<Vector2>();
-        }
-
-        return Vector2.zero;
+        var mouseScreenPos = Input.mousePosition;
+        Debug.Log("mouseScreenPos: " + mouseScreenPos);
+        mouseScreenPos.z = camera.WorldToScreenPoint(transform.position).z;
+        return camera.ScreenToWorldPoint(mouseScreenPos);
     }
 
     private void Update()
     {
-        //if (!moveAllowed) return;
+        if (!moveAllowed) return;
 
-        //Vector2 MouseDelta = GetMouseLookInput();
-
-        //MouseDelta *= speed * Time.deltaTime;
-        
-        //transform.Translate(Vector3.right * (inverted ? 1 : -1), Space.Self);
-        //transform.Translate(Vector3.up * (inverted ? 1 : -1), Space.Self);
+        transform.position = WorldPos + offset;
     }
+
+    private IEnumerator Drag()
+    {
+        moveAllowed = true;
+        Vector3 offset = transform.position - WorldPos;
+        // grab
+        GetComponent<Rigidbody>().useGravity = false;
+        while (moveAllowed)
+        {
+            // dragging
+            transform.position = WorldPos + offset;
+            yield return null;
+        }
+        // drop
+        GetComponent<Rigidbody>().useGravity = true;
+    }*/
 }
--- Assets/Code/MountAstrolabio/RotateAstrolabio.cs
+++ Assets/Code/MountAstrolabio/RotateAstrolabio.cs
@@ -1,27 +1,8 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class RotateAstrolabio : MonoBehaviour
 {
-    #region Input Actions
-    [SerializeField]
-    private InputActionAsset inputActions;
-
-    public InputActionAsset actions
-    {
-        get => inputActions;
-        set => inputActions = value;
-    }
-
-    protected InputAction leftClickInputAction { get; set; }
-
-    protected InputAction mouseLookInputAction { get; set; }
-
-#endregion
-
     #region Variables
-
-    private bool rotateAllowed;
 
     private Camera camera;
 
@@ -31,63 +12,24 @@ public class RotateAstrolabio : MonoBehaviour
 
     #endregion
 
-    private void Awake()
-    {
-        InitializeInputSystem();
-    }
-
     private void Start()
     {
         camera = Camera.main;
     }
 
-    private void InitializeInputSystem()
+    private void OnMouseDown()
     {
-        leftClickInputAction = actions.FindAction("Left Click");
-        if (leftClickInputAction != null)
-        {
-            leftClickInputAction.started += OnLeftClickPressed;
-            leftClickInputAction.performed += OnLeftClickPressed;
-            leftClickInputAction.canceled += OnLeftClickPressed;
-        }
-
-        mouseLookInputAction = actions.FindAction("Mouse Look");
-
-        actions.Enable();
+        Debug.Log("DOWN");
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    protected virtual void OnLeftClickPressed(InputAction.CallbackContext context)
+    private void OnMouseDrag()
     {
-        if (context.started || context.performed)
-        {
-            rotateAllowed = true;
-            Cursor.lockState = CursorLockMode.Locked;
-        }
-        else if (context.canceled)
-        {
-            rotateAllowed = false;
-            Cursor.lockState = CursorLockMode.None;
-        }
-    }
+        Debug.Log("Drag");
+        float mouseY = Input.GetAxis("Mouse Y");
+       
+        float rotation = mouseY * speed * Time.deltaTime;
 
-    protected virtual Vector2 GetMouseLookInput()
-    {
-        if (mouseLookInputAction != null)
-        {
-            return mouseLookInputAction.ReadValue<Vector2>();
-        }
-
-        return Vector2.zero;
-    }
-
-    private void Update()
-    {
-        if (!rotateAllowed) return;
-
-        Vector2 MouseDelta = GetMouseLookInput();
-
-        MouseDelta *= speed * Time.deltaTime;
-
-        transform.Rotate(Vector3.up * (inverted ? 1 : -1), MouseDelta.x, Space.World);
+        transform.Rotate(Vector3.up, inverted ? rotation : -rotation, Space.World);
     }
 }
